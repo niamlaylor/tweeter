ab463426d4e5d44483024cacdc493fb4cd239e08
Liam Naylor <liam@liamnaylor.com>
2022-11-22 15:34:50 -0800

    Update readme and screenshots + eslint js files

--- README.md
+++ README.md
@@ -2,17 +2,28 @@
 
 Tweeter is a simple, single-page Twitter clone.
 
-This repository is the starter code for the project: Students will fork and clone this repository, then build upon it to practice their HTML, CSS, JS, jQuery and AJAX front-end skills, and their Node, Express back-end skills.
+Post what's on your mind 😎
+
+!["Tweeter mobile view"](./public/images/tweeter-mobile.png)
+!["Tweeter desktop view"](./public/images/tweeter-desktop.png)
 
 ## Getting Started
 
-1. [Create](https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template) a new repository using this repository as a template.
-2. Clone your repository onto your local device.
-3. Install dependencies using the `npm install` command.
+1. Clone this repository onto your local device.
+2. Install dependencies using the `npm install` command.
 3. Start the web server using the `npm run local` command. The app will be served at <http://localhost:8080/>.
 4. Go to <http://localhost:8080/> in your browser.
+
+## Features
+
+- Post tweets to your feed
+- A live character counter helps you keep your tweet short enough
+- Tweeter has a reponsive design built as mobile-first
 
 ## Dependencies
 
 - Express
 - Node 5.10.x or above
+- Body-parser
+- Chance
+
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,66 +1,60 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+const maxTweetLength = 140; // Characters
+const slideSpeed = 200; // Milliseconds - speed of slideUp/slideDown jquery animations
 
-  const maxTweetLength = 140; // Characters
-  const slideSpeed = 200; // Milliseconds - speed of slideUp/slideDown jquery animations
+// This function prevents malicious entries in the tweet textarea
+const escape = function(str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
-  // This function prevents malicious entries in the tweet textarea
-  const escape = function (str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
+const createTweetElement = function(tweet) {
+  const timeAgo = timeago.format(tweet.created_at);
+  const safeHTML = `<p>${escape(tweet.content.text)}</p>`;
 
-  const createTweetElement = function(tweet) {
-    const timeAgo = timeago.format(tweet.created_at);
-    const safeHTML = `<p>${escape(tweet.content.text)}</p>`
-
-    const $tweet = `<article class="tweet-box">
-    <header class="tweet-box-header">
-      <div>
-        <img alt="user-icon" src="${tweet.user.avatars}" alt="user-icon">
-        <h3 class="tweet-box-username">${tweet.user.name}</h3>
+  const $tweet = `<article class="tweet-box">
+  <header class="tweet-box-header">
+    <div>
+      <img alt="user-icon" src="${tweet.user.avatars}" alt="user-icon">
+      <h3 class="tweet-box-username">${tweet.user.name}</h3>
+    </div>
+    <h3 class="tweet-box-header-handle">${tweet.user.handle}</h3>
+  </header>
+  ${safeHTML}
+  <footer class="tweet-box-footer">
+    <div>
+      <p class="tweet-box-footer-days">From ${timeAgo}</p>
+      <div class="tweet-box-footer-icons">
+        <a><i class="fa-solid fa-flag"></i></a>
+        <a><i class="fa-solid fa-retweet"></i></a>
+        <a><i class="fa-solid fa-heart"></i></a>
       </div>
-      <h3 class="tweet-box-header-handle">${tweet.user.handle}</h3>
-    </header>
-    ${safeHTML}
-    <footer class="tweet-box-footer">
-      <div>
-        <p class="tweet-box-footer-days">From ${timeAgo}</p>
-        <div class="tweet-box-footer-icons">
-          <a><i class="fa-solid fa-flag"></i></a>
-          <a><i class="fa-solid fa-retweet"></i></a>
-          <a><i class="fa-solid fa-heart"></i></a>
-        </div>
-      </div>
-    </footer>
-  </article>`;
-    return $tweet;
+    </div>
+  </footer>
+</article>`;
+  return $tweet;
+};
+
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    $('#tweets-container').prepend(createTweetElement(tweet));
   }
+};
 
-  const renderTweets = function(tweets) {
-    for (const tweet of tweets) {
-      $('#tweets-container').prepend(createTweetElement(tweet));
-    };
+// This function returns an appropriate error string based on the error type
+const throwError = function(errorType) {
+  const errors = {
+    empty: `Tweet field cannot be empty.`,
+    tooLong: `Tweet must be ${maxTweetLength} characters or less.`
   };
+  const $errorMessage = `${errors[errorType]}`;
+  return $errorMessage;
+};
 
-  // This function returns an appropriate error string based on the error type
-  const throwError = function(errorType) {
-    const errors = {
-      empty: `Tweet field cannot be empty.`,
-      tooLong: `Tweet must be ${maxTweetLength} characters or less.`
-    }
-    const $errorMessage = `${errors[errorType]}`
-    return $errorMessage;
-  };
-
-  // Whenever we run any jQuery, we need it to be inside .ready
-$(document).ready( function() {
+// Whenever we run any jQuery, we need it to be inside .ready
+$(document).ready(function() {
   // Ajax request for submitting the create tweet form
-  $('#new-tweet-form').submit( function(event) {
+  $('#new-tweet-form').submit(function(event) {
     event.preventDefault();
     const tweetData = $(this).serialize();
 
@@ -77,18 +71,18 @@ $(document).ready( function() {
       })
       .then(() => {
         $('#new-tweet-error').slideUp(slideSpeed);
-      })
-      })
+      });
+      });
     // Below are error checks for the compose tweet field
     } else if (tweetLength > maxTweetLength) {
       const errorType = 'tooLong';
       const $errorMessage = throwError(errorType);
-      $('#new-tweet-error-text').text($errorMessage)
+      $('#new-tweet-error-text').text($errorMessage);
       $('#new-tweet-error').slideDown(slideSpeed);
     } else if (!tweetLength) {
       const errorType = 'empty';
       const $errorMessage = throwError(errorType);
-      $('#new-tweet-error-text').text($errorMessage)
+      $('#new-tweet-error-text').text($errorMessage);
       $('#new-tweet-error').slideDown(slideSpeed);
     }
   });
@@ -96,7 +90,7 @@ $(document).ready( function() {
   const loadTweets = function() {
     $.get('/tweets', function(data, status) {
       status === 'success' ? renderTweets(data) : console.log(status);
-    })
-  }
+    });
+  };
   loadTweets();
 });
